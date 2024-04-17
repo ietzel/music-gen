a541c3269f011b4c19614bb1e4a1e91deb648644
ietzel <67884277+ietzel@users.noreply.github.com>
2024-04-17 13:06:04 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,9 +15,42 @@ random_device rd;
 mt19937 gen(rd());
 uniform_real_distribution<> dis(0.0, 1.0);
 
-string[] tonics = [1, 4, 5];
-string[] dominants = [2, 8, 11, 12];
-string[] predominants = [3, 6, 7, 9, 10];
+int[] tonics = [1, 4, 5];
+int[] dominants = [2, 8, 11, 12];
+int[] predominants = [3, 6, 7, 9, 10];
+
+int[3][][] chords = [
+  [
+    [1, 15, 17, 20], //Madd9 (a wind bell)
+    [1, 8, 13, 17, 18, 20], //Madd11 (christian women)
+    [1, 8, 15, 20, 22, 24, 25], //M13 omit 3rd (black hole sun)
+    [1, 5, 6, 8, 13, 17, 20, 22], //Msus4sus13 (love of my life)
+    [],
+    [],
+    [],
+    []
+  ],
+  [ 
+    [5, 18, 22, 25], //Mb13b9 omit 3rd, omit 5th, omit 7th (a wind bell)
+    [1, 10, 15, 19, 20, 22], M13#11 omit 3rd, omit 7th (christian women)
+    [1, 8, 15, 20, 22, 24, 25], //M13 omit 3rd (black hole sun)
+    [1, 11, 17, 23], //7 omit 5th (love of my life)
+    [],
+    [],
+    [],
+    []
+  ],
+  [  
+    [11, 25, 28, 33], //11 omit 3rd, omit 5th (a wind bell)
+    [1, 15, 20, 24, 25, 27], //M9 omit 3rd (christian women)
+    [1, 8, 15, 20, 22, 24, 25], //M13 omit 3rd (black hole sun)
+    [1, 8, 11, 17], //m7 (love of my life)
+    [],
+    [],
+    [],
+    []
+  ]
+];
 
 string chords(ratio) {
   string chord = 
